366959118720409b43d7e28ac126be870eac9309
Maxine Zick <25956545+MaxTheMooshroom@users.noreply.github.com>
2026-07-30 06:41:52 -0400

    some minor reworking

--- flake.nix
+++ flake.nix
@@ -1,87 +1,95 @@
 {
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    lib = {
-      flake = false;
-      url = ./lib/top-level.nix;
-    };
-  };
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
   outputs =
     { self, flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      {
-        config,
-        lib,
-        mlib,
-        ...
-      }:
-      {
-        systems = lib.systems.flakeExposed;
+    flake-parts.lib.mkFlake
+      { inherit inputs; }
+      (
+        { lib, mlib, ... }:
+        {
+          systems = lib.systems.flakeExposed;
 
-        imports = with flake-parts.flakeModules; [
-          flakeModules
-          partitions
+          imports =
+            with flake-parts.flakeModules;
+              [
+                flakeModules
+                partitions
 
-          (
-            { config, ... }:
-            {
-              _module.args.mlib = config.flake.lib;
-            }
-          )
-        ];
-
-        flake.lib = import inputs.lib lib;
-
-        partitionedAttrs = {
-          devShells = "dev";
-          formatter = "dev";
-        };
-
-        flake.flakeModules =
-          let
-            prefixAttrNames =
-              prefix:
-              lib.mapAttrsToList (
-                name: value: {
-                  inherit value;
-                  name = "${prefix}-${name}";
-                }
-              );
-
-            mergeAttrNamesDown = mlib.turn builtins.listToAttrs (
-              mlib.turn builtins.concatLists (
-                lib.mapAttrsToList (
-                  name: value:
-                  if builtins.isAttrs value then prefixAttrNames name value else [ { inherit name value; } ]
+                (
+                  { config, ... }:
+                  {
+                    _module.args.mlib = config.flake.lib;
+                  }
                 )
-              )
-            );
-          in
-          mergeAttrNamesDown mlib.modules.flake-parts;
+              ];
 
-        partitions.dev = {
-          extraInputsFlake = ./dev;
+          flake.lib = import ./lib lib;
 
-          module =
-            { inputs, ... }:
+          partitionedAttrs =
             {
-              perSystem =
-                { pkgs, ... }:
-                {
-                  formatter = pkgs.nixfmt-tree;
+              devShells = "dev";
+              formatter = "dev";
+            };
 
-                  devShells.default = pkgs.mkShellNoCC {
-                    packages = with pkgs; [
-                      nixfmt
-                      nixfmt-tree
-                      # ...
-                    ];
-                  };
+          flake.flakeModules =
+            let
+              prefixAttrNames =
+                prefix:
+                lib.mapAttrsToList
+                  (
+                    name: value:
+                    {
+                      inherit value;
+                      name = "${prefix}-${name}";
+                    }
+                  );
+
+              mergeAttrNamesDown =
+                mlib.turn
+                  builtins.listToAttrs
+                  (
+                    mlib.turn
+                      builtins.concatLists
+                      (
+                        lib.mapAttrsToList
+                          (
+                            name: value:
+                            if    builtins.isAttrs value
+                            then  prefixAttrNames name value
+                            else  [ { inherit name value; } ]
+                          )
+                      )
+                  );
+            in
+              mergeAttrNamesDown mlib.modules.flake-parts;
+
+          partitions.dev =
+            {
+              extraInputsFlake = ./dev;
+
+              module =
+                { inputs, ... }:
+                {
+                  perSystem =
+                    { pkgs, ... }:
+                    {
+                      formatter = pkgs.nixfmt-tree;
+
+                      devShells.default =
+                        pkgs.mkShellNoCC
+                          {
+                            packages =
+                              with pkgs;
+                              [
+                                nixfmt
+                                nixfmt-tree
+                                # ...
+                              ];
+                          };
+                    };
                 };
             };
-        };
-      }
-    );
+        }
+      );
 }
